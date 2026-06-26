11c2a1746072aee0e8b620009b19d5c8624c5d38
star <nklandalt@gmail.com>
2026-06-26 00:52:48 -0500

    Removed star's bot card, as it is discontinued.

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,13 +10,6 @@ export const projectSections = [
     subtitle: "Stuff I've made.",
     cards: [
       {
-        name: "star's bot",
-        desc: "Discord bot application - basic QoL stuff lol",
-        tags: ["Discord Bot", "JavaScript"],
-        link: "https://discord.com/users/1500680986164203720",
-        thumb: "https://i.imgur.com/HBZExRo.png"
-      },
-      {
         name: "LexBot",
         desc: "Another discord bot i made but better than stars bot",
         tags: ["Discord Bot", "JavaScript"],
